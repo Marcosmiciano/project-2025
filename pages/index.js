64093ea7ebc12435975089dc41f7cbdf957c9104
Marcos Antônio Miciano Rafael <60630415+Marcosmiciano@users.noreply.github.com>
2025-11-24 02:14:19 +0000

    alterar arquivo index

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,4 @@ function Home() {
   return <h1>Pagina Inicial | ByteShield Corp</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste() {
-    console.log("teste");
-}
 export default Home;
